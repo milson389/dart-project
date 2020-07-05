e0a5439e48e1fc6951936d83656e4e2431341c61
milson389 <mugetsu3008@gmail.com>
2020-07-06 04:57:51 +0700

    Dart language Functions

---
+++ Belajar Dart/src/Functions.dart
@@ -0,0 +1,34 @@
+void main(){
+  greet('Audie', 2000);
+
+  var firstNumber = 7;
+  var secondNumber = 10;
+
+  print('Rata rata dari $firstNumber & $secondNumber adalah ${average(firstNumber, secondNumber)}');
+  print('Rata rata dari $firstNumber & $secondNumber adalah ${rata2(firstNumber, secondNumber)}');
+
+  greetNewUser();
+
+}
+
+void greet( String name, bornYear){
+  var age = 2020 - bornYear;
+  print('Halo $name, Tahun ini Anda berusia $age tahun');
+}
+
+void greetNewUser({String name = "Dicoding", int age = 20, bool isVerified = false}){
+  print('Halo $name, tahun ini kamu berusia $age ya?');
+  if( age > 18 ){
+    isVerified = true;
+    print('Wah berarti kamu status verifiednya : $isVerified');
+  }else{
+    var usia = 18 - age;
+    print('Wah tinggal $usia tahun lagi status verified kamu jadi : ${!isVerified}');
+  }
+}
+
+double average(num num1, num num2){
+  return( num1 + num2 ) / 2;
+}
+
+double rata2(num num1, num num2) => ( num1 + num2 ) / 2; // arrow expression
