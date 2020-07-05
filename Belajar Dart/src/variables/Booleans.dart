613ff65a8e1df6803a1f7c92b8ff60940c0856c2
milson389 <mugetsu3008@gmail.com>
2020-07-06 04:10:52 +0700

    learning booleans variables

---
+++ Belajar Dart/src/variables/Booleans.dart
@@ -0,0 +1,17 @@
+void main(){
+  bool alwaysTrue = true;
+  var alwaysFalse = false;
+  var notTrue = !true;
+  bool notFalse = !false;
+
+  print(alwaysTrue);
+  print(alwaysFalse);
+  print(notTrue);
+  print(notFalse);
+
+  if(true){
+    print("It's true");
+  }else{
+    print("It's false");
+  }
+}
