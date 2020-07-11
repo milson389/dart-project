5960ced09075f2dc1f3819f8b8aea9041744652e
milson389 <mugetsu3008@gmail.com>
2020-07-11 21:02:21 +0700

    While Loops

---
+++ Belajar Dart/src/controlFlow/challenge/inputUserName.dart
@@ -0,0 +1,19 @@
+import 'dart:io';
+
+void main(){
+  String username;
+  bool notValid = false;
+
+  do{
+    stdout.write('Masukkan nama Anda (min. 6 karakter): ');
+    username = stdin.readLineSync();
+
+    if( username.length < 6){
+      notValid = true;
+      print('Username Anda tidak valid');
+    }else{
+      break;
+    }
+
+  }while(notValid);
+}
---
+++ Belajar Dart/src/controlFlow/loops/whileDoWhileLoops.dart
@@ -0,0 +1,21 @@
+import 'dart:io';
+
+void main(){
+  var i = 1;
+  var j = 1;
+
+  // while loops
+  print('While Loops : ');
+  while( i <= 5 ){
+    print(i);
+    i++;
+  }
+
+  // do while loops
+  print(' ');
+  print('Do While Loops : ');
+  do{
+    print(j);
+    j++;
+  }while( j <= 5 );
+}
