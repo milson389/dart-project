b74aac8145bb9cc1ae076bd02fa40779db6c7ca4
milson389 <mugetsu3008@gmail.com>
2020-07-12 06:51:06 +0700

    applying enum to switch case

---
+++ Belajar Dart/src/oop/Enumeration.dart
@@ -0,0 +1,32 @@
+// enum mewakili kumpulan konstan yang membuat kode kita lebih mudah dibaca
+
+enum Rainbow{
+  red, orange, yellow, green, blue, indigo, violet
+}
+
+enum Status{
+  Todo, In_Progress, In_Review, Done
+}
+
+void main(){
+  print(Rainbow.values);
+  print(Rainbow.blue);
+  print(Rainbow.orange.index);
+
+  var taskStatus = Status.In_Progress;
+
+  switch(taskStatus){
+    case Status.Todo :
+      print('Task is still in To Do');
+      break;
+    case Status.In_Progress:
+      print('Task is in progress');
+      break;
+    case Status.In_Review:
+      print('Task is currenly under review');
+      break;
+    case Status.Done:
+      print('Task is done');
+      break;
+  }
+}
