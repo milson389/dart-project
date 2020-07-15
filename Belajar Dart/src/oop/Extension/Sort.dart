7e8573275c68c28ca93c78c94c56d97545020940
milson389 <mugetsu3008@gmail.com>
2020-07-15 18:52:51 +0700

    Adding Selection Sort Algorithm as an Extension Method

---
+++ Belajar Dart/src/oop/Extension/Sort.dart
@@ -0,0 +1,21 @@
+// Selection Sort Algorithm
+
+extension Sorting on List<int>{
+  List<int> sortAsc(){
+    var list = this;
+    var length = this.length;
+
+    for( int i = 0; i < length; i++ ){
+      int min = i;
+      for( int j = i + 1; j < length; j++){
+        if( list[j] < list[min]){
+          min = j;
+        }
+      }
+      int tmp = list[min];
+      list[min] = list[i];
+      list[i] = tmp;
+    }
+    return list;
+  }
+}
